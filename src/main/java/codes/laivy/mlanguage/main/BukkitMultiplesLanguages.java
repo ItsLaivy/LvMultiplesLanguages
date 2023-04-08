24ce06367bb691a139fe33ae20f9e654cca7d451
Laivy <dnlfg.contato@gmail.com>
2023-04-08 19:47:45 -0300

    Removed debugs

--- src/main/java/codes/laivy/mlanguage/main/BukkitMultiplesLanguages.java
+++ src/main/java/codes/laivy/mlanguage/main/BukkitMultiplesLanguages.java
@@ -60,56 +60,51 @@ public class BukkitMultiplesLanguages extends JavaPlugin implements Platform<Plu
         if (isServerLoaded()) api.load();
     }
 
-    @EventHandler
-    private void itemChange(@NotNull ItemTranslateEvent e) {
-        e.setName(Objects.requireNonNull(getApi().getStorage(this, "Nome teste")).get("Teste3"));
-        e.setLore(Objects.requireNonNull(getApi().getStorage(this, "Nome teste")).get("Teste3"));
-    }
-
-    @EventHandler
-    private void chat(@NotNull AsyncPlayerChatEvent e) {
-        Bukkit.getScheduler().runTask(this, () -> {
-            try {
-                int slot = Integer.parseInt(e.getMessage());
-                EntityPlayer.getEntityPlayer(e.getPlayer()).getConnection().sendPacket(getDefApi().getVersion().createSetSlotPacket(0, slot, -1, codes.laivy.mlanguage.api.bukkit.reflection.classes.item.ItemStack.getNMSItemStack(new ItemStack(Material.DIAMOND))));
-            } catch (NumberFormatException ignore) {
-                if (e.getMessage().equals("a")) {
-                    NBTTagCompound compound = codes.laivy.mlanguage.api.bukkit.reflection.classes.item.ItemStack.getNMSItemStack(e.getPlayer().getItemInHand()).getTag();
-                    Bukkit.broadcastMessage(compound.getValue().toString());
-                } else if (e.getMessage().equals("get")) {
-                    BukkitMessageStorage storage = (BukkitMessageStorage) Objects.requireNonNull(getApi().getStorage(this, "Nome teste"));
-
-                    if (ReflectionUtils.isCompatible(V1_9_R1.class)) {
-                        e.getPlayer().getInventory().setItem(45, new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.DIAMOND_PICKAXE), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                    }
-                    Set<ItemStack> items = new LinkedHashSet<>();
-                    for (org.bukkit.inventory.ItemStack ignore2 : e.getPlayer().getInventory()) {
-                        int random = new Random().nextInt(5);
-                        Material material;
-                        if (random == 0) {
-                            material = Material.DIAMOND;
-                        } else if (random == 1) {
-                            material = Material.REDSTONE;
-                        } else if (random == 2) {
-                            material = Material.COAL;
-                        } else if (random == 3) {
-                            material = Material.GOLD_INGOT;
-                        } else {
-                            material = Material.IRON_INGOT;
-                        }
-
-                        items.add(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(material, new Random().nextInt(64) + 1), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                    }
-                    e.getPlayer().getInventory().setContents(items.toArray(new org.bukkit.inventory.ItemStack[0]));
-
-                    e.getPlayer().getInventory().setHelmet(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_HELMET), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                    e.getPlayer().getInventory().setChestplate(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_CHESTPLATE), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                    e.getPlayer().getInventory().setLeggings(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_LEGGINGS), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                    e.getPlayer().getInventory().setBoots(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_BOOTS), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
-                }
-            }
-        });
-    }
+    // TODO: 08/04/2023 Debug only
+//    @EventHandler
+//    private void chat(@NotNull AsyncPlayerChatEvent e) {
+//        Bukkit.getScheduler().runTask(this, () -> {
+//            try {
+//                int slot = Integer.parseInt(e.getMessage());
+//                EntityPlayer.getEntityPlayer(e.getPlayer()).getConnection().sendPacket(getDefApi().getVersion().createSetSlotPacket(0, slot, -1, codes.laivy.mlanguage.api.bukkit.reflection.classes.item.ItemStack.getNMSItemStack(new ItemStack(Material.DIAMOND))));
+//            } catch (NumberFormatException ignore) {
+//                if (e.getMessage().equals("a")) {
+//                    NBTTagCompound compound = codes.laivy.mlanguage.api.bukkit.reflection.classes.item.ItemStack.getNMSItemStack(e.getPlayer().getItemInHand()).getTag();
+//                    Bukkit.broadcastMessage(compound.getValue().toString());
+//                } else if (e.getMessage().equals("get")) {
+//                    BukkitMessageStorage storage = (BukkitMessageStorage) Objects.requireNonNull(getApi().getStorage(this, "Nome teste"));
+//
+//                    if (ReflectionUtils.isCompatible(V1_9_R1.class)) {
+//                        e.getPlayer().getInventory().setItem(45, new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.DIAMOND_PICKAXE), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                    }
+//                    Set<ItemStack> items = new LinkedHashSet<>();
+//                    for (org.bukkit.inventory.ItemStack ignore2 : e.getPlayer().getInventory()) {
+//                        int random = new Random().nextInt(5);
+//                        Material material;
+//                        if (random == 0) {
+//                            material = Material.DIAMOND;
+//                        } else if (random == 1) {
+//                            material = Material.REDSTONE;
+//                        } else if (random == 2) {
+//                            material = Material.COAL;
+//                        } else if (random == 3) {
+//                            material = Material.GOLD_INGOT;
+//                        } else {
+//                            material = Material.IRON_INGOT;
+//                        }
+//
+//                        items.add(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(material, new Random().nextInt(64) + 1), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                    }
+//                    e.getPlayer().getInventory().setContents(items.toArray(new org.bukkit.inventory.ItemStack[0]));
+//
+//                    e.getPlayer().getInventory().setHelmet(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_HELMET), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                    e.getPlayer().getInventory().setChestplate(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_CHESTPLATE), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                    e.getPlayer().getInventory().setLeggings(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_LEGGINGS), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                    e.getPlayer().getInventory().setBoots(new TranslatableBukkitItem(new org.bukkit.inventory.ItemStack(Material.LEATHER_BOOTS), new BukkitMessage(storage, "Teste1"), new BukkitMessage(storage, "Teste2")).getItem());
+//                }
+//            }
+//        });
+//    }
 
     @Override
     public void log(@NotNull BaseComponent component) {
